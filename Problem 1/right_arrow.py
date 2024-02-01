0a53d87096847d8b264c26b35c191017acde32f9
rns8657 <rns8657@utulsa.edu>
2024-02-01 14:53:07 -0600

    second try?

--- Problem 1/right_arrow.py
+++ Problem 1/right_arrow.py
@@ -5,12 +5,13 @@ def right_arrow():
     row1 = '      ' + head_char
     row2 = str(6*base_char) + str(2*head_char)
     row3 = str(6*base_char) + str(3*head_char)
+    last_row = str(6*base_char) + str(head_char)
 
     print(row1)
     print(row2)
     print(row3)
     print(row2)
-    print(row1)
+    print(last_row)
 
 if __name__ == "__main__":
     right_arrow()
